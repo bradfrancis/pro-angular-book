3535629d18ec5ada023dea91c41d38d25ed3ad66
Brad Francis <brad.francis.90@gmail.com>
2017-04-21 10:32:43 +1000

    Started Chapter 8. Completed up to Listing 8-7. Commence from section ‘Adding URL Routing’.

--- chapter8/SportsStore/app/store/store.component.ts
+++ chapter8/SportsStore/app/store/store.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { Product } from "../model/product.model";
 import { ProductRepository } from "../model/product.repository";
+import { Cart } from "../model/cart.model";
 
 @Component({
     selector: "store",
@@ -12,7 +13,8 @@ import { ProductRepository } from "../model/product.repository";
     public productsPerPage = 4;
     public selectedPage = 1;
     
-    constructor(private repository: ProductRepository) { }
+    constructor(private repository: ProductRepository,
+                private cart: Cart) { }
     
     get products(): Product[] {
         let pageIndex = (this.selectedPage - 1) * this.productsPerPage
@@ -34,11 +36,6 @@ import { ProductRepository } from "../model/product.repository";
         this.changePage(1);
     }
 
-//    get pageNumbers(): number[] {
-//        return Array(Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage))
-//            .fill(0).map((x, i) => i + 1);
-//    }
-
     get pageCount(): number {
         return Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage)
     }
@@ -46,5 +43,9 @@ import { ProductRepository } from "../model/product.repository";
     changeCategory(newCategory?: string) {
         this.selectedCategory = newCategory;
         this.changePage(1);
+    }
+                   
+    addProductToCart(product: Product) {
+        this.cart.addLine(product);
     }
  }
