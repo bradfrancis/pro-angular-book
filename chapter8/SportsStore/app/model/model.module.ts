195056db4a507b4552ce79bb49da3876d52d68b4
bradfrancis <brad.francis.90@gmail.com>
2017-04-24 00:47:48 +1000

    Completed Chapter 8.

--- chapter8/SportsStore/app/model/model.module.ts
+++ chapter8/SportsStore/app/model/model.module.ts
@@ -2,8 +2,22 @@ import { NgModule } from "@angular/core";
 import { ProductRepository } from "./product.repository";
 import { StaticDataSource } from "./static.datasource";
 import { Cart } from "./cart.model";
+import { Order } from "./order.model";
+import { OrderRepository } from "./order.repository";
+import { HttpModule } from "@angular/http"
+import {RestDataSource} from "./rest.datasource";
 
 @NgModule({
-    providers: [ProductRepository, StaticDataSource, Cart]
+    imports: [HttpModule],
+    providers: [
+        ProductRepository,
+        Cart,
+        Order,
+        OrderRepository,
+        {
+            provide: StaticDataSource,
+            useClass: RestDataSource
+        }
+    ]
 })
  export class ModelModule { }
--- chapter8/SportsStore/app/store/cartDetail.component.ts
+++ chapter8/SportsStore/app/store/cartDetail.component.ts
@@ -1,6 +1,11 @@
 import { Component } from "@angular/core";
+import { Cart } from "../model/cart.model";
 
 @Component({
-    template: `<div><h3 class="bg-info p-a-1">Cart Detail Component</h3></div>`
+    templateUrl: "cartDetail.component.html",
+    moduleId: module.id
 })
-export class CartDetailComponent {}
+export class CartDetailComponent {
+
+    constructor(public cart: Cart) { }
+}
